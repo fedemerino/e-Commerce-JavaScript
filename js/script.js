a56bacb253d1f2271ff8fba3521daa235f900c45
Federico Merino <fedemerino00@gmail.com>
2022-08-02 23:27:53 -0300

    entrega 2

--- js/script.js
+++ js/script.js
@@ -6,12 +6,12 @@ const productos=[
 
     },
     {
-        prod: "mouse",
+        prod: "Mouse Logitech G203 RGB",
         precio: 3999,
         stock: 5
     },
     {
-        prod: "teclado",
+        prod: "Teclado Logitech G Pro RGB",
         precio: 12499,
         stock: 0
     }
@@ -21,10 +21,10 @@ const productos=[
 
 function checkStock(producto,i){
     if(productos[i].stock===0){
-        console.log("El producto: " +productos[i].prod + " no tiene stock.")
+        alert("El producto: " +productos[i].prod + " no tiene stock.")
     }
     else {
-        console.log("El producto " +productos[i].prod + " está en stock");
+        alert("El producto " +productos[i].prod + " está en stock");
     }
 }
 
@@ -33,7 +33,7 @@ function calcularIntereses(precio, producto){
     let precioInteres=precio;
    while(i<=12){
         precioInteres*=1.3;
-        console.log("El producto " +producto + " cuesta $" +(precioInteres.toFixed(2))   + " en " +i+" cuotas. Cada cuota tiene un valor de " +((precioInteres/i)).toFixed(2));
+        alert("El producto " +producto + " cuesta $" +(precioInteres.toFixed(2))   + " en " +i+" cuotas. Cada cuota tiene un valor de " +((precioInteres/i)).toFixed(2));
         i+=3;
     }
 }
@@ -43,6 +43,15 @@ var prod = prompt("Ingrese el producto que desea comprar: \nTeclado\nMouse\nAuri
 prod = prod.toLowerCase();
 }while((prod != "teclado") && (prod!="mouse") && (prod!="auriculares"));
 
+function confirmarCompra(){
+    let confirm = prompt("Desea confirmar la compra? (s/n)")
+    if (confirm==="s"){
+        let nombre = prompt("Ingrese su nombre y apellido");
+        let correo = prompt("Ingrese su correo electronico");
+        alert("Muchas gracias " + nombre + ". Recibirá las instrucciones para continuar su compra en el siguiente correo: " +correo);
+    }
+    else{alert("Gracias por visitarnos!")}}
+
 let pteclado=2;
 let pmouse=1;
 let pauris=0;
@@ -50,12 +59,18 @@ let pauris=0;
 if(prod=="teclado"){
     checkStock(productos[2],pteclado);
     calcularIntereses(productos[2].precio, productos[2].prod);
+    if (productos[2].stock > 0){
+        confirmarCompra();}
 }
 else if(prod=="auriculares"){
     checkStock(productos[0],pauris);
     calcularIntereses(productos[0].precio, productos[0].prod);
+    if (productos[0].stock > 0){
+        confirmarCompra();}
 }
 else {
     checkStock(productos[1],pmouse);
     calcularIntereses(productos[1].precio, productos[1].prod);
+    if (productos[1].stock > 0){
+    confirmarCompra();}
 }
