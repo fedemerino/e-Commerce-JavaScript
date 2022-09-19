575eb425b2f3645f6997edf1b056be8cc6c279a9
Federico Merino <fedemerino00@gmail.com>
2022-09-19 00:51:47 -0300

    carrito y totales terminados
    agregar evento en boton comprar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-
+//HOLA
 let carrito = [];
 var productosJSON = [];
 let contenedor = document.getElementById('cards');
 let tablaRender = document.getElementById('itemsTabla');
+let tablaFooter = document.querySelector('#Footer');
 let contenedorCarrito = document.querySelector("#items");
 let contenedorFooterCarrito = document.querySelector("#footer")
 let totalItems = document.querySelector("#totalItems")
 let acum = 0;
+let acumTotal = 0;
 let card = document.getElementsByClassName('card');
 const searchBar = document.getElementById('searchBar')
 const botonComprar = document.getElementById('comprar');
@@ -17,10 +19,9 @@ const filtrarTodos = document.querySelectorAll('.filterTodos')
 let productosFiltrados = []
 const estandarDolaresAmericanos = Intl.NumberFormat('es-AR');
 
+// FETCH JSON LOCAL
 
 fetchJSON();
-
-// FETCH JSON LOCAL
 
 async function fetchJSON() {
     const URLJSON = "../js/prod.json"
@@ -33,8 +34,7 @@ async function fetchJSON() {
 //RENDERIZAR PRODUCTOS Y LE ASIGNO A CADA BOTON UN EVENTO PARA AGREGAR CADA PRODUCTO AL CARRO
 
 const renderProductos = (prod) => {
-    const productos = prod
-        .map((prod) => {
+    const productos = prod.map((prod) => {
             return `
             <div class="col-lg-3">
             <div class=" card ${prod.categoria} todos ">
@@ -52,14 +52,13 @@ const renderProductos = (prod) => {
         })
         .join('')
     contenedor.innerHTML = productos;
-    
+
     for (let x in prod) {
         document.getElementById(`btn${prod[x].id}`).addEventListener("click", function () {
             agregarAlCarrito(prod[x])
         })
-        
-    }
-    mostrarLocal(prod);
+    };
+    mostrarLocal();
 };
 
 
@@ -78,68 +77,11 @@ searchBar.addEventListener('keyup', (e) => {
 
 // RENDERIZO EN EL CARRITO EL LOCALSTORAGE EN CASO DE TENER ALGO GUARDADO
 
-function mostrarLocal(prod){
-
-if (localStorage.getItem("productosEnCarro")) {
-    carrito = JSON.parse(localStorage.getItem("productosEnCarro"))
-    mostrarEnCarrito(prod);
-
-    function mostrarEnCarrito(prod){
-        let totalCarrito = 0;
-        tablaRender.innerHTML = '';
-        carrito.forEach((prod) => {
-        let renglonCarrito = document.createElement("tr");
-        renglonCarrito.innerHTML =
-            `
-        <div style="display:flex; text-align:center;">
-            <td style="margin: 0 3rem 0 3rem; padding: 1rem"><img src="${prod.img}" width="96" height="64"></td>
-            <td style="margin: 0 3rem 0 3rem">${prod.prod}</td>
-            <td style="margin: 0 3rem 0 3rem; text-align:center;"><input type="number" id="prodCantidad-${prod.id}" style="color:black;width: 3rem; padding: 0.2rem; margin: 0rem; text-align:center;" min = "1" max = "9" step="1" value="${prod.cantidad}" ></td>
-            <td style="margin: 0 3rem 0 3rem">${estandarDolaresAmericanos.format(prod.precio * prod.cantidad)}</td>
-            <td style="margin: 0 3rem 0 3rem"><img src="../assets/delete.png" style="width: 2.2rem;height: 3rem;"></td>
-        </div>
-        `;
-            tablaRender.append(renglonCarrito);
-        })
+function mostrarLocal() {
     
-        let inputCantidadProductos = document.getElementById(`prodCantidad-${prod.id}`);
-        inputCantidadProductos.addEventListener('change', (e) => {
-            let nuevaCantidad = e.target.value;
-            prod.cantidad = nuevaCantidad;
-            console.log(nuevaCantidad);
-            mostrarEnCarrito(prod);
-        });
-
-
-        totalItems.innerHTML =
-        
-            `
-    <div style="text-align:end; margin-right:2.5rem; margin-bottom: 0.5rem;"> 
-        <td style="margin: 0 3rem 0 3rem">${acum}</td>
-    </div>
-    `;
-    }
-
-    /* for (let prod of carrito) {
-        tablaRender.innerHTML +=
-            `
-        <div style="display:flex; text-align:center;" id="${prod.id}">
-            <td style="margin: 0 3rem 0 3rem";><img src="${prod.img}" width="96" height="64"></td>
-            <td style="margin: 0 3rem 0 3rem">${prod.prod}</td>
-            <td style="margin: 0 3rem 0 3rem; text-align:center;"><input type="number" class="prodCantidad" style="color:black;width: 3rem; padding: 0.2rem; margin: 0rem; text-align:center;" min = "1" max = "9" step="1" value="${prod.cantidad}" ></td>
-            <td style="margin: 0 3rem 0 3rem">${estandarDolaresAmericanos.format(prod.precio * prod.cantidad)}</td>
-            <td style="margin: 0 3rem 0 3rem"><img src="../assets/delete.png" style="width: 2rem;"></td>
-        </div>
-        `; 
-
-        acum += prod.precio*prod.cantidad;
-
-        totalItems.innerHTML =
-            `
-        <div style="text-align:end; margin-right:2.5rem; margin-bottom: 0.5rem;"> 
-            <td style="margin: 0 3rem 0 3rem">${acum}</td>
-        </div>
-        `;*/
+    if (localStorage.getItem("productosEnCarro")) {
+        carrito = JSON.parse(localStorage.getItem("productosEnCarro"))
+        mostrarEnCarrito();
     }
 }
 
@@ -150,10 +92,65 @@ function cambioCantidad(prod) {
 }
 
 
+// RENDERIZAR CARRITO
 
+function mostrarEnCarrito() {
+    let acumTotal = 0;
+    tablaRender.innerHTML = '';
+    carrito.forEach((prod) => {
+        let renglonCarrito = document.createElement("tr");
+        renglonCarrito.innerHTML =
+            `
+    <div style="display:flex; text-align:center;">
+        <td style="margin: 0 3rem 0 3rem; padding: 1rem"><img src="${prod.img}" width="96" height="64"></td>
+        <td style="margin: 0 3rem 0 3rem">${prod.prod}</td>
+        <td style="margin: 0 3rem 0 3rem; text-align:center;"><input type="number" id="prodCantidad-${prod.id}" style="color:black;width: 3rem; padding: 0.2rem; margin: 0rem; text-align:center;" min = "1" max = "9" step="1" value="${prod.cantidad}" ></td>
+        <td style="margin: 0 3rem 0 3rem">$${estandarDolaresAmericanos.format(prod.precio * prod.cantidad)}</td>
+        <td style="margin: 0 3rem 0 3rem; width: 3rem;"><button class="btn" id="eliminarProducto-${prod.id}">🗑️</button></td>
+    </div>
+    `;
+        tablaRender.append(renglonCarrito); 
+        acum = prod.precio * prod.cantidad;
+        prod.precioTotal = acum ;
+        
+    })
+
+    for (let prod of carrito){
+        acumTotal += prod.precioTotal
+    }  
+    
+    if(carrito.length == 0){
+        totalItems.innerHTML = "";
+    }
+    else {
+        totalItems.innerHTML =
+
+        `
+        <div style="text-align:end; margin-right:2.5rem; margin-bottom: 0.5rem;"> 
+        <td style="margin: 0 3rem 0 3rem">$${estandarDolaresAmericanos.format(acumTotal)}</td>
+        </div>
+        `;
+    }
+
+        carrito.forEach((prod) => {
+            document.getElementById(`prodCantidad-${prod.id}`).addEventListener('change', (e) => {
+                let nuevaCantidad = e.target.value;
+                prod.cantidad = nuevaCantidad;
+                console.log(nuevaCantidad);    
+                mostrarEnCarrito(prod);})
+            document.getElementById(`eliminarProducto-${prod.id}`).addEventListener('click', function(){
+                eliminarProductoDelCarrito(prod.id)
+                if(carrito.length == 0){
+                    totalItems.innerHTML = "";
+                }
+                localStorage.removeItem('productosEnCarro');
+                localStorage.setItem("productosEnCarro", JSON.stringify(carrito))
+            })
+            
+            });
+}
 
 //AGREGAR PRODUCTOS AL CARRITO
-
 
 function agregarAlCarrito(prod) {
 
@@ -168,48 +165,11 @@ function agregarAlCarrito(prod) {
     if (existe) {
         Swal.fire('¡Este producto ya se encuentra en el carrito!')
     }
-    else { 
+    else {
         carrito.push(prod);
         localStorage.setItem("productosEnCarro", JSON.stringify(carrito))
         swal.fire("Agregaste " + prod.prod + " al carrito")
-        mostrarEnCarrito(prod);}
-        
-
-
-    function mostrarEnCarrito(prod){
-        let totalCarrito = 0;
-        tablaRender.innerHTML = '';
-        carrito.forEach((prod) => {
-        let renglonCarrito = document.createElement("tr");
-        renglonCarrito.innerHTML =
-            `
-        <div style="display:flex; text-align:center;">
-            <td style="margin: 0 3rem 0 3rem; padding: 1rem"><img src="${prod.img}" width="96" height="64"></td>
-            <td style="margin: 0 3rem 0 3rem">${prod.prod}</td>
-            <td style="margin: 0 3rem 0 3rem; text-align:center;"><input type="number" id="prodCantidad-${prod.id}" style="color:black;width: 3rem; padding: 0.2rem; margin: 0rem; text-align:center;" min = "1" max = "9" step="1" value="${prod.cantidad}" ></td>
-            <td style="margin: 0 3rem 0 3rem">${estandarDolaresAmericanos.format(prod.precio * prod.cantidad)}</td>
-            <td style="margin: 0 3rem 0 3rem"><img src="../assets/delete.png" style="width: 2.2rem;height: 3rem;"></td>
-        </div>
-        `;
-            tablaRender.append(renglonCarrito);
-        })
-    
-        let inputCantidadProductos = document.getElementById(`prodCantidad-${prod.id}`);
-        inputCantidadProductos.addEventListener('change', (e) => {
-            let nuevaCantidad = e.target.value;
-            prod.cantidad = nuevaCantidad;
-            console.log(nuevaCantidad);
-            mostrarEnCarrito(prod);
-        });
-
-
-        totalItems.innerHTML =
-        
-            `
-    <div style="text-align:end; margin-right:2.5rem; margin-bottom: 0.5rem;"> 
-        <td style="margin: 0 3rem 0 3rem">${acum}</td>
-    </div>
-    `;
+        mostrarEnCarrito();
     }
 
     console.log(carrito)
@@ -220,9 +180,18 @@ function agregarAlCarrito(prod) {
 
 botonLimpiar.addEventListener("click", function () {
     localStorage.removeItem('productosEnCarro');
+    carrito = [];
     tablaRender.innerHTML = "";
     totalItems.innerHTML = "";
 })
+
+//ELIMINAR PRODUCTOS
+const eliminarProductoDelCarrito = (prodId) => {
+    const producto = carrito.find((prod) => prod.id === prodId);
+    const index = carrito.indexOf(producto);
+    carrito.splice(index, 1);
+    mostrarEnCarrito();
+}
 
 
 //FILTRO PRODUCTOS CON LOS BOTONES
